269fb779919acee1a8be5a1954013aa956d86131
melchor629 <melchor9000@gmail.com>
2018-01-05 11:53:56 +0100

    Workaround for daemon using a huge amount of CPU

--- Playbox.widget/lib/getcurrenttrack.sh
+++ Playbox.widget/lib/getcurrenttrack.sh
@@ -2,14 +2,29 @@
 
 lib=$(pwd)/Playbox.widget/lib
 GetCurrentTrack="$lib/GetCurrentTrack"
+
+function wait_daemon {
+    #Wait untile the pidfile is created
+    while [ ! -f "$lib/pidfile"]; do
+        python -c "import time; time.sleep(0.1)"
+    done
+    #When is created, wait a bit, just in case
+    python -c "import time; time.sleep(0.1)"
+}
+
 if [[ ! -f "$lib/pidfile" ]]; then
+    #Wait until the daemon is running
     "$GetCurrentTrack" -d
+    wait_daemon
 elif ps -p $(cat "$lib/pidfile") > /dev/null; then
-    echo -n " " #Is running
+    echo -n " "
 else
+    #Wait until the daemon is running
     rm "$lib/pidfile"
     "$GetCurrentTrack" -d
+    wait_daemon
 fi
 
 echo > /tmp/get_current_track
 cat < /tmp/get_current_track
+
