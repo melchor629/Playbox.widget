bf9eb18eed5174621483f6cd822e2a2ec7cf6a61
melchor629 <melchor9000@gmail.com>
2018-10-04 23:19:29 +0200

    Add alert in readme for Mojave

--- service/GetCurrentTrack/main.m
+++ service/GetCurrentTrack/main.m
@@ -54,45 +54,7 @@ static void run(bool daemonized) {
     }
 }
 
-//https://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux/17955149#17955149
-static void daemonize(void) {
-    pid_t pid = fork();
-    if(pid < 0) {
-        checc(pid, "Could not create the daemon");
-        exit(EXIT_FAILURE);
-    } else if(pid > 0) {
-        //Parent must exist right now
-        exit(EXIT_SUCCESS);
-    }
-
-    if(setsid() < 0) exit(EXIT_FAILURE);
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
-    //Fork off for the second time
-    pid = fork();
-    if(pid < 0) {
-        checc(pid, "Could not create the daemon (2)");
-        exit(EXIT_FAILURE);
-    } else if(pid > 0) {
-        //Child must exist right now
-        exit(EXIT_SUCCESS);
-    }
-    umask(0);
-    for(int x = (int) sysconf(_SC_OPEN_MAX); x >= 0; x--){
-        close(x);
-    }
-}
-
 int main(int argc, const char * argv[]) {
-    bool should_daemonize = false;
-    if(argc == 2) {
-        should_daemonize = !strcmp("-d", argv[1]);
-    }
-
-    if(should_daemonize) daemonize();
-
-    run(should_daemonize);
-
+    run(false);
     return 0;
 }
